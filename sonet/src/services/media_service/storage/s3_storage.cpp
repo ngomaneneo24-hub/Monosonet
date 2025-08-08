b9e4f832227d3ab3610d86268a949cc091be163e
Neo Qiss <devqiss@gmail.com>
2025-08-08 23:23:55 +0000

    Add initial implementation of media service with README and storage placeholders

---
+++ sonet/src/services/media_service/storage/s3_storage.cpp
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2025 Neo Qiss
+// All rights reserved.
+//
+// This software is proprietary and confidential.
+// Unauthorized copying, distribution, or use is strictly prohibited.
+//
+
+// Placeholder for S3/R2/MinIO backend. To be implemented with AWS SDK or MinIO SDK.
